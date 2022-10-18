654ddacd30a5205b668944f7cf1263ca0f69f9e1
Emiliano Ruiz Carletti <contact@eruizc.dev>
2022-10-17 22:24:21 -0300

    Refactor navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,9 +5,17 @@ export interface NavbarProps {
   scrolling?: boolean;
 }
 
-const Navbar = (props: NavbarProps) => {
+const Logo = () => (
+  <a
+    style={{ cursor: 'pointer' }}
+    onClick={() => document.documentElement.scrollTo({ top: 0, behavior: 'smooth' })}
+  >
+    <Image src="/XMLID-15.svg" alt="Neovim" className="h-14" width={40} height={40} />
+  </a>
+);
+
+const RegisterPrompt = () => {
   const [email, setEmail] = useState('');
-  const { scrolling } = props;
 
   const handleChangeEmail = useCallback(
     (event: ChangeEvent<HTMLInputElement>) => {
@@ -16,41 +24,33 @@ const Navbar = (props: NavbarProps) => {
     []
   );
 
-  const scrollingClass = scrolling ? 'bg-black' : 'bg-green-500';
+  return (
+    <div className="hidden items-center space-x-2 text-sm">
+      <label htmlFor="email">Register:</label>
+      <input
+        type="text"
+        placeholder="Enter your email"
+        name="email"
+        value={email}
+        onChange={handleChangeEmail}
+        className={`rounded p-1 bg-white/20 outline-none border border-white/10 placeholder:text-gray-500`}
+      />
+    </div>
+  );
+};
+
+const Navbar = (props: NavbarProps) => {
+  const { scrolling } = props;
 
   return (
     <nav
       className={`fixed top-0 right-0 left-0 h-16
         flex items-center px-4 justify-between text-gray-800
         transition duration-300
-        ${scrollingClass}`}
+        ${scrolling ? 'bg-black' : 'bg-green-500'}`}
     >
-      <a
-        style={{ cursor: 'pointer' }}
-        onClick={() =>
-          document.documentElement.scrollTo({ top: 0, behavior: 'smooth' })
-        }
-      >
-        <Image
-          src="/XMLID-15.svg"
-          alt="Neovim"
-          className="h-14"
-          width={40}
-          height={40}
-        />
-      </a>
-      <div className="hidden items-center space-x-2 text-sm">
-        <label htmlFor="email">Register:</label>
-        <input
-          type="text"
-          placeholder="Enter your email"
-          name="email"
-          value={email}
-          onChange={handleChangeEmail}
-          className={`rounded p-1 bg-white/20 outline-none 
-            border border-white/10 placeholder:text-gray-500`}
-        />
-      </div>
+      <Logo />
+      <RegisterPrompt />
     </nav>
   );
 };
