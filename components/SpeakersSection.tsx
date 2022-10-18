48eb74e3af883d41a3b5b5412c164ae5cea0be8e
Emiliano Ruiz Carletti <contact@eruizc.dev>
2022-10-18 19:47:19 -0300

    Add some speakers

--- components/SpeakersSection.tsx
+++ components/SpeakersSection.tsx
@@ -1,7 +1,19 @@
+const Speaker = () => {
+  return (
+    <>
+      <p>Emi, Age 23</p>
+    </>
+  );
+};
+
 const SpeakersSection = () => {
   return (
     <section className="py-16">
       <h2 className="">Speakers</h2>
+      <Speaker />
+      <Speaker />
+      <Speaker />
+      <Speaker />
     </section>
   );
 };
