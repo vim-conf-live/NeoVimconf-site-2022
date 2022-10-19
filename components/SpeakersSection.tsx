81d9f2669fc666b39a1072606233354bd5a2ada7
Emiliano Ruiz Carletti <contact@eruizc.dev>
2022-10-18 22:05:00 -0300

    add your card

--- components/SpeakersSection.tsx
+++ components/SpeakersSection.tsx
@@ -1,5 +1,22 @@
 import speakers, { Speaker } from "../speakers";
 import Image from 'next/future/image';
+import Link from "next/link";
+
+const YouCard = () => {
+  return (
+    <>
+      <Link href="https://forms.gle/hU18jSEWL2r9xmqP9" target="_blank">
+        <a className="rounded-lg bg-black m-2 p-2 flex flex-row" target="_blank">
+          <Image className="rounded-full mr-4" src="http://www.clipartbest.com/cliparts/Kij/oAb/KijoAbjzT.png" alt="idk" width={60} height={60} />
+          <div className="flew">
+            <h3 className="text-green-600 text-xl font-medium leading-tight mb-2">You!</h3>
+            <p className="">Click to submit your presentation</p>
+          </div>
+        </a>
+      </Link>
+    </>
+  );
+}
 
 const SpeakerCard = (speaker: Speaker) => {
   return (
@@ -18,6 +35,7 @@ const SpeakersSection = () => {
     <section>
       <h2>Speakers</h2>
       <div className="flex flex-col">
+        <YouCard />
         {speakers.map(s => <SpeakerCard { ...s } />)}
       </div>
     </section>
